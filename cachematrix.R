2ff7111ebc3b19904681a0c19b073d6da1faa777
Ammar Imad <87394607+ammar-im@users.noreply.github.com>
2022-10-27 19:04:07 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The belwo functions calculate the Inverse of a matrix
+## and stores it in the cache, so if the submitted matrix has it is Inverse
+## value already calculated, then the function will get the value from cache.
+## Otherwise, it will compute the value and store it in the cache
 
-## The below function contains the sub-functions that cache a matrix
+## The below function contains the sub-functions that are necessary 
+## to cache a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
         Inv <- NULL
@@ -19,7 +22,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## the below function gets the inverse of a given matrix and cache it using the
-## MakeCacheMatrix functions
+## MakeCacheMatrix functions, if the matrix is already submitted, the function
+## will get the inverse from the cache. Otherwise, it will compute it and store
+## it in the cache.
 
 cacheSolve <- function(x, ...) {
         Inv <- x$getInverse()
